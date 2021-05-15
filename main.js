2fea64f820cbdc0e1277c4a2c1fa6639feff7178
Alba V <albavidalm@gmail.com>
2021-05-15 20:54:04 +0200

    Add random number

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+console.log("Working");
+
+//Genera Random Number e imprime en la consola
+const randomNumber = getRandomNumber(100);
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+console.log(randomNumber);
+
+//_________________________
